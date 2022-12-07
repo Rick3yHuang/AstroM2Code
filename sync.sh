fb2c4cbcc8dc4013a22713c10ccfeded97f7210e
Rick3yHuang <rickey.huang747@gmail.com>
2022-12-06 23:10:49 -0500

    Updated on 2022-12-06 23:10:49

--- sync.sh
+++
@@ -1,4 +0,0 @@
-cp -r "/Users/rickeyhuang/Desktop/Fall 2022/Astro-Geometry Group/notes/Overleaf/6318ab61068465040f5d9988/M2" "/Users/rickeyhuang/Desktop/Repositories/AstroM2Code"
-git add .
-git commit -m "Updated on `date +'%Y-%m-%d %H:%M:%S'`"
-git push
